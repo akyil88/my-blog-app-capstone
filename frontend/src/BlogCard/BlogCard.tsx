2ed10522feefe52b6557c6837516564c82aad733
Gökhan Akyil <akyil88@outlook.de>
2024-07-04 21:56:39 +0300

    BlogCard Style

--- frontend/src/BlogCard/BlogCard.tsx
+++ frontend/src/BlogCard/BlogCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './BlogCard.css';
-import { Blog } from "./Blog.ts";
+import { Blog } from "./Blog";
 import { Link } from "react-router-dom";
 
 type BlogCardProps = {
@@ -10,19 +10,20 @@ type BlogCardProps = {
 
 const BlogCard: React.FC<BlogCardProps> = (props) => {
     return (
-        <div className="blog-card">
-            {props.blog.image && (
-                <div className="image-preview">
-                    <img src={`data:image/jpeg;base64,${props.blog.image}`} alt="Blog Image"/>
-
-
-                    <Link to={`/blog/${props.blog.id}`}>
-                    <h3 className="blog-title">{props.blog.title}</h3></Link>
+        <div className="container">
+            <div className="blog-card">
+                {props.blog.image && (
+                    <div className="image-preview">
+                        <img src={`data:image/jpeg;base64,${props.blog.image}`} alt="Blog Image" />
+                    </div>
+                )}
+                <div className="blog-content">
+                    <Link to={`/blog/${props.blog.id}`} className="blog-title">
+                        <h3>{props.blog.title}</h3>
+                    </Link>
                     <p className="blog-description">{props.blog.description}</p>
-
                 </div>
-            )}
-
+            </div>
         </div>
     );
 };
